bb570b51cbbd4a9c754a9c4ae88db04086bfe789
Clement Delafargue <clement@delafargue.name>
2016-08-02 14:46:29 +0200

    Display markers for last position and make the sub path yellow instead of blue

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,3 +1,9 @@
+var colors = {
+  'boat-1': 'blue',
+  'boat-2': 'blue',
+  'sub': 'yellow'
+};
+
 var mymap = L.map('mapid').setView([49.975, -4.00], 9);
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
@@ -21,7 +27,15 @@ var fetchJson = function(path) {
 
 fetchJson('/api/path').then(function(points) {
   for(var boat of Object.keys(points)) {
-    addPolylineToMap(points[boat].map(function(point) { return point.point}), {});
+    var boatPoints = points[boat];
+    var lastPoint = boatPoints[boatPoints.length - 1];
+    addPolylineToMap(boatPoints.map(function(point) { return point.point}),  { color: colors[boat] });
+    var marker = L.marker([lastPoint.point.x, lastPoint.point.y]).addTo(mymap);
+    if(boat == 'sub') {
+      marker.bindPopup(boat).openPopup();
+    } else {
+      marker.bindPopup(boat);
+    }
   }
   // ToDo display depth graph
 });
