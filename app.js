3c47b91ee49caad2d8aa1e3b60615a99ab8a402c
Clement Delafargue <clement@delafargue.name>
2016-08-08 10:01:00 +0200

    Less verbose logging in prod settings

--- app.js
+++ app.js
@@ -16,7 +16,6 @@ app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
 app.use(compression());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -37,6 +36,7 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
+  app.use(logger('dev'));
   app.use(function(err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
